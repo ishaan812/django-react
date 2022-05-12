81d6fb1551bbf9be059d4d01715409bd008bc85b
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-05-13 02:32:44 +0530

    first commit

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+function App() {
+  return (
+    <div>
+        <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
+const appdiv=document.getElementById("app")
+render(<App/>,appdiv);
