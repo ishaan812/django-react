1ecf1652b56563b21976b74097c4fc2d38201e2a
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2022-05-16 15:25:25 +0530

    new main

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -10,7 +10,7 @@ const Landing=()=>{
                         <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
                             <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
                                 Validate your  
-                                <span className="text-indigo-700"> Hobby1</span>
+                                <span className="text-indigo-700"> Hobby</span>
                             </h1>
                             <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">A professonal website drives sales. Create a beautiful website to impress and engage new customers and establish your business online </p>
                         </div>
